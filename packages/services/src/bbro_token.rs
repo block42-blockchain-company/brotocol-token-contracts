b163ed760806bdd23304ac3cffc5c5af694f461c
noln <mark22dom@gmail.com>
2022-03-12 13:39:49 +0300

    migrate msg for bbro token

---
+++ packages/services/src/bbro_token.rs
@@ -0,0 +1,8 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+/// ## MigrateMsg
+/// This structure describes a migration message.
+/// We currently take no arguments for migrations.
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct MigrateMsg {}
